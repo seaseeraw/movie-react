1db2a137b012f549e638ba9694302bf4cb0a1ef5
seaseeraw <seaseeraw12@gmail.com>
2024-10-09 10:54:08 +1100

    hello

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,8 @@ export const Hero = () => {
 const [searchMovie, setSearchMovie]=useState({});
 const [bgImg, setBgImg]=useState("");
 const shouldFetchRef = useRef(true);
-const searchRef = useRef("")
+const searchRef = useRef("");
+const [searching, setSearching] = useState(false);
 useEffect(()=>{
     if(shouldFetchRef.current){
         fetchMovie(randomChar());
@@ -22,8 +23,8 @@ const fetchMovie = async (str) =>{
     const movie = await FetchApiData(str);
     setSearchMovie(movie);
     setBgImg(movie.Poster);
-
-}
+setSearching(false);
+};
 
 const handleOnMovieSearch =()=>{
     const str =searchRef.current.value;
@@ -47,20 +48,30 @@ const movieStyle ={
             className='container'>Welcome To Movie World</h2>
         </nav>
         <div className="hero d-flex justify-content-center align-items-center text-light" style={movieStyle}>
+
             <div className='hero-content'>
-            <div className="form-center">
-                <div className="text-center">
-                    <h1>Search Movies On Your Demand</h1>
-                    <p>Explore Movie</p>
-                </div>
-            </div>
+            <div className={searching?"form-center":"form-top"}>
+{
+    searching &&  
+    
+    <div className="text-center">
+        <h1>Search Movies On Your Demand</h1>
+        <p>Explore Movie</p>
+    </div>
+
+}
             <div className="input-group my-5">
-  <input  ref={searchRef} type="text" className="form-control" placeholder="Search Movie By Name" aria-label="Search Movie By Name" aria-describedby="button-addon2"/>
+  <input  ref={searchRef} onFocus={()=>setSearching(true)}type="text" className="form-control" placeholder="Search Movie By Name" aria-label="Search Movie By Name" aria-describedby="button-addon2"/>
   <button onClick={handleOnMovieSearch}className="btn btn-danger" type="button" id="button-addon2">Search</button>
 </div>
-<div className="movie-card-display">
+</div>
+{/* checking condition  */}
+{!searching && (
+    <div className="movie-card-display showMovie">
     <MovieCard searchMovie={searchMovie}/>
 </div>
+)}
+
 
 </div>
         </div>
